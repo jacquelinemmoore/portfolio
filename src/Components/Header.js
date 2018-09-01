ea88b40f3926703393724c2bcea7303c9d21067e
Jacqueline Moore <jacmoore@ucdavis.edu>
2018-09-01 16:58:18 -0700

    adding content

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -36,7 +36,9 @@ class Header extends Component {
                 <div className="row banner">
                     <div className="banner-text">
                         <h1 className="responsive-headline">I'm {name}.</h1>
-                        <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+                        <h3>I'm a <span>{occupation}</span> from {city}, California.</h3>
+                        <h3>I'm also {description}</h3>
+                        <h3>(That's me in the picture.)</h3>
                         <hr/>
                         <ul className="social">
                             {networks}
